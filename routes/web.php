5024750aad3a8d81c1ac374bf04ac619b6edc813
18521 <18521619@gm.uit.edu.vn>
2021-11-15 21:25:19 +0700

    push login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+Route::get('active/{token}', 'Auth\RegisterController@activation')->name('active_account');
+Route::namespace('Pages')->group(function () {
+    Route::get('/', 'HomePage')->name('home_page');
 });
