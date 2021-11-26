89ec323f3dc367a05b3c392be60c132a16dd58fc
18521 <18521619@gm.uit.edu.vn>
2021-11-26 21:47:40 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 Route::get('active/{token}', 'Auth\RegisterController@activation')->name('active_account');
 Route::get('/home', 'App\Http\Controllers\Pages\HomePage')->name('home_page');
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+*/
+Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('admin')
+    ->group(function() {
+        Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+    });
